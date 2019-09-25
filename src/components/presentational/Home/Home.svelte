8147900742067f04c20ffdd28080da9707c2fd21
Rafael Câmara <rafaelcamaram@gmail.com>
2019-09-25 19:44:40 -0300

    Adding svelte project

--- src/components/presentational/Home/Home.svelte
+++ src/components/presentational/Home/Home.svelte
@@ -1,5 +1,6 @@
 <script>
-  import IllustrationMain from "../../../assets/images/IllustrationMain.svelte";
+  import NavBar from "../NavBar";
+  import Logo from "../../../assets/images/Logo.svelte";
   export let name = "John";
 </script>
 
@@ -7,27 +8,31 @@
 
 </style>
 
+<NavBar />
 <div id="home-container">
-
-  <div id="two-side-container__left">
-    <IllustrationMain />
+  <div id="featured-logo">
+    <Logo width="50px" />
+    <p>2019 _ Edição 2</p>
   </div>
-
-  <div id="two-side-container__right">
-    <div id="two-side-container__right--content">
-
-      <h1>All your files in one secure location, accessible anywhere</h1>
-      <p>
-        Fylo stores all your most important files in one secure location. Access
-        them wherever you need, share and collaborate with friends, family and
-        co-workers.
-      </p>
-
-      <h3>Sign up for early access</h3>
-      <div id="two-side-container__email">
-        <input />
-        <button>Sign Up</button>
-      </div>
-    </div>
+  <div id="featured_images">
+    <div id="featured-image" class="icon__close" />
+    <div id="detail-image" class="icon__close" />
   </div>
 </div>
+
+<div id="about-container">
+  <div class="content">
+    <h2 class="background-title">Sobre a hubcon</h2>
+    <p>Sobre a hubcon_</p>
+    <h3>
+      A hubcon é um evento de tecnologia lorem fringilla vel, aliquet nec,
+      vulputate eget, arcu ut, imperdiet a, venenatis vitae, justo. Pede mollis
+      pretium.
+    </h3>
+  </div>
+</div>
+
+<p>More content...</p>
+<p>More content...</p>
+<p>More content...</p>
+<p>More content...</p>
