213d74617b8c0db5a1c8928cee34fc218c3c7121
Nelson Sinis <nelsonsinis@ioasys.com.br>
2019-10-02 23:09:54 -0300

    Fixed layout

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from '../logo.svg';
-import galaxy from './galaxy.gif';
-import './Hero.css'
+import React from "react";
+import logo from "../logo.svg";
+import galaxy from "./galaxy.gif";
+import "./Hero.css";
 
-const Hero = () => <section id="inicio">
-  <div className='container'>
-    <div className='hero-columns'>
-      <div className='logo-content'>
-        <img src={logo} />
-      </div>
-      <div className='geom-content'>
-        <div className='formats'>
-          <div className='geometric-formats-big'>
-            <img src={galaxy} />
+const Hero = () => (
+  <section id="inicio">
+    <div className="container hero-container">
+      <div className="hero-columns">
+        <div className="logo-content">
+          <img src={logo} />
+          <div>
+            <h6>2019 _ EDIÇÃO 2</h6>
           </div>
-          <div className='geometric-formats-small' />
+        </div>
+        <div className="geom-content">
+          <div className="formats">
+            <div className="geometric-formats-big">
+              <img src={galaxy} />
+            </div>
+            <div className="geometric-formats-small" />
+            <div id="date-hubcon" />
+            <div id="schedule-hubcon">
+              <h6>DEZ 07, 2019</h6>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
+);
 
 export default Hero;
